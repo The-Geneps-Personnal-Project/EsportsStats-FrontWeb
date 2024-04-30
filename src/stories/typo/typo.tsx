4c4543926fb3a1e613a31f1078091ba8de39b25d
Julien Delphine <julien.delphine@epitech.eu>
2024-04-30 09:39:43 +0200

    ✨ feat: add overide header typography to storybook

---
+++ src/stories/typo/typo.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+export const Typo: React.FC = () => {
+  return (
+    <>
+      <h1 className="mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce est
+        tellus, sollicitudin ac posuere nec, ultricies vitae arcu. Nullam
+        maximus leo est. Aliquam a urna nisi.
+      </h1>
+      <h2 className="mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce est
+        tellus, sollicitudin ac posuere nec, ultricies vitae arcu. Nullam
+        maximus leo est. Aliquam a urna nisi.
+      </h2>
+      <h3 className="mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce est
+        tellus, sollicitudin ac posuere nec, ultricies vitae arcu. Nullam
+        maximus leo est. Aliquam a urna nisi.
+      </h3>
+      <h4 className="mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce est
+        tellus, sollicitudin ac posuere nec, ultricies vitae arcu. Nullam
+        maximus leo est. Aliquam a urna nisi.
+      </h4>
+      <h5 className="mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce est
+        tellus, sollicitudin ac posuere nec, ultricies vitae arcu. Nullam
+        maximus leo est. Aliquam a urna nisi.
+      </h5>
+      <h6 className="mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce est
+        tellus, sollicitudin ac posuere nec, ultricies vitae arcu. Nullam
+        maximus leo est. Aliquam a urna nisi.
+      </h6>
+    </>
+  );
+};

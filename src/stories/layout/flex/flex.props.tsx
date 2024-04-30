1a22a585758e3e9ae35c7863acdd175cef1bdaa5
Julien Delphine <julien.delphine@epitech.eu>
2024-04-30 17:30:09 +0200

    ✨ feat: add flex div as component

---
+++ src/stories/layout/flex/flex.props.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export type FlexProps = {
+  children: React.ReactNode;
+  justifyContent?:
+    | "flex-start"
+    | "flex-end"
+    | "center"
+    | "space-between"
+    | "space-around"
+    | "space-evenly";
+  alignItems?: "stretch" | "flex-start" | "flex-end" | "center" | "baseline";
+  flexDirection?: "row" | "row-reverse" | "column" | "column-reverse";
+} & React.HTMLAttributes<HTMLDivElement>;
